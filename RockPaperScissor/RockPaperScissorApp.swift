d9c0193a6ef63ba46d68e66d93366bb0c1eb207e
Raymond Chen <ahrchen@gmail.com>
2021-09-22 20:55:44 -0700

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Raymond Chen on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
