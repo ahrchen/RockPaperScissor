a42fd48237c7549603993c411ecfccba7c3fee64
Raymond Chen <ahrchen@gmail.com>
2021-10-06 20:54:21 -0700

    Adding Projects from lessons found here https://www.hackingwithswift.com/100/swiftui

--- RockPaperScissor/ContentView.swift
+++ RockPaperScissor/ContentView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let moves = ["Rock", "Paper", "Scissors"]
+    @State var playerScore = 0
+    @State var round = 1
+    @State var appChoice = Int.random(in: 0..<3)
+    @State var shouldWin = Bool.random()
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(){
+            Text(round >= 10 ? "Score: \(playerScore)/\(round)" : "Round: \(round)")
+            Text("CPU is \(moves[appChoice])")
+                .padding(.bottom)
+            
+            Text(shouldWin ? "You Should Win" :
+            "You Should Lose")
+        
+            HStack {
+                ForEach(0..<3) { number
+                    in Button(action: {self.buttonTapped(number)}) {
+                        Text(moves[number])
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    func buttonTapped(_ number: Int) {
+        var isWin: Bool? = nil
+        
+        if appChoice + 1 == number  {
+            isWin = true
+        } else if appChoice == 2 && number == 0 {
+            isWin = true
+        } else if appChoice - 1 == number {
+            isWin = false
+        } else if appChoice == 0 && number == 2 {
+            isWin = false
+        }
+        
+        if shouldWin && (isWin != nil) && isWin!{
+            playerScore += 1
+        } else if !shouldWin && (isWin != nil) && !isWin! {
+            playerScore += 1
+        }
+        
+        round += 1
+        appChoice = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
     }
 }
 
